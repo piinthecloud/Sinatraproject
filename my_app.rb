3651e26674aae3898f814ecb4c97cb7c52dfe07f
stephanie pi <stephmpi@gmail.com>
2014-09-25 12:21:04 -0700

    added stuffs

--- my_app.rb
+++ my_app.rb
@@ -1,11 +1,19 @@
 require 'sinatra'
-
+require './lib/posts'
 
 
 class MyApp < Sinatra::Base
   # This is where your code will go
   #get takes a string argument (a path in the url) and a block of code. could be /about_me
+
+  before do
+
+    #you can put assignments here to apply to all routes
+  end
+
+
   get "/" do
+    @post = Post.splitarray
 #    erb method looks for an html file and it reads it
     erb :index
   end
@@ -19,7 +27,7 @@ class MyApp < Sinatra::Base
   end
 
   get "/posts/:postname" do
-    erb("/posts/#{params[:postname]}".to_sym)
+    erb "posts/#{params[:postname]}".to_sym
   end
 
 end
