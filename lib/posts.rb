c2db326b59dbb0581258baa922886d0441bb65ab
stephanie pi <stephmpi@gmail.com>
2014-09-29 19:58:29 -0400

    test changes

--- lib/posts.rb
+++ lib/posts.rb
@@ -1,7 +1,7 @@
-require "date"
+# require "date"
 
 class Post
-  attr_accessor :title, :url, :date
+  attr_accessor :title, :url
 
 #access posts, files withing views/posts
 #Attributes: title, content, path, filename
@@ -12,16 +12,14 @@ class Post
 #Post.all => [post, post]
   def initialize(array)
     @title = array[-1].split(".")[0]
-    # @date = Date.parse(array[-2])
     @url = "/posts/#{@title}"
-#/#{@date}
 
   end
 
 
   def self.all
     #an each method would not return an array of arrays, while the collect method actually returns the array
-    Dir.glob("views/posts/*/*")
+    Dir.glob("views/posts/*")
   end
 
   def self.splitarray
