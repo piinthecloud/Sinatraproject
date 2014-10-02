0240a763a73bb42eefc50d47cd64854b5dead5a4
stephanie pi <stephmpi@gmail.com>
2014-10-02 13:55:40 -0700

    add rounded edges on header

---
+++ public/javascripts/app.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   $("#links a").click(function() {
+//     $(this).css("color", "blue");
+//     // var fontSize = $(this).css("font-size");
+//     // $(this).css("fontSize", (Math.random() * 100) + "px");
+//
+//   });
+//
+// });
